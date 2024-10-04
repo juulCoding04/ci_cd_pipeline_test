4bddb65909a7d9ee0a629373eb1b5f1aee79b693
Juul <juul20018@gmail.com>
2024-10-04 22:22:45 +0200

    adding some code to a test branch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int sum(int a, int b);
+
 int main() {
+    int a=1;
+    int b=2;
+    int s;
     std::cout << "Hello World!" << std::endl;
+    s = sum(a, b);
+    std::cout << s << std::endl;
     return 0;
+}
+
+int sum(int a, int b) {
+    return a+b;
 }
