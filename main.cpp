8324deb82849cab0b7495697a670a7be0cbd5ead
juulCoding04 <juulCoding04@users.noreply.github.com>
2024-10-07 13:36:27 +0000

    chore: fix linting issues

--- main.cpp
+++ main.cpp
@@ -5,29 +5,23 @@ int sub(int a, int b);
 int mul(int a, int b);
 
 int main() {
-    int a=1;
-    int b=2;
-    int s;
-    int min;
-    int mult;
-    std::cout << "Hello World!" << std::endl;
-    s = sum(a, b);
-    min = sub(a, b);
-    mult = mul(a, b);
-    std::cout << s << std::endl;
-    std::cout << min << std::endl;
-    std::cout << mult << std::endl;
-    return 0;
+  int a = 1;
+  int b = 2;
+  int s;
+  int min;
+  int mult;
+  std::cout << "Hello World!" << std::endl;
+  s = sum(a, b);
+  min = sub(a, b);
+  mult = mul(a, b);
+  std::cout << s << std::endl;
+  std::cout << min << std::endl;
+  std::cout << mult << std::endl;
+  return 0;
 }
 
-int sum(int a, int b) {
-    return a+b;
-}
+int sum(int a, int b) { return a + b; }
 
-int sub(int a, int b) {
-    return b-a;
-}
+int sub(int a, int b) { return b - a; }
 
-int mul(int a, int b) {
-    return a*b;
-}
+int mul(int a, int b) { return a * b; }
