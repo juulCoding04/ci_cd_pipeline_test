62d8c75c7a831c193da49685380f85709df1a5da
Juul <juul20018@gmail.com>
2024-10-07 14:36:16 +0200

    new dummy code for linting test

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 
 int sum(int a, int b);
 int sub(int a, int b);
+int mul(int a, int b);
 
 int main() {
     int a=1;
     int b=2;
     int s;
     int min;
+    int mult;
     std::cout << "Hello World!" << std::endl;
     s = sum(a, b);
     min = sub(a, b);
+    mult = mul(a, b);
     std::cout << s << std::endl;
     std::cout << min << std::endl;
+    std::cout << mult << std::endl;
     return 0;
 }
 
@@ -22,4 +26,8 @@ int sum(int a, int b) {
 
 int sub(int a, int b) {
     return b-a;
+}
+
+int mul(int a, int b) {
+    return a*b;
 }
